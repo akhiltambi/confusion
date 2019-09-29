acb5a6467323c13df1bbff702dfd0d6b3157e5ba
Akhil Tambi <akhiltambi@gmail.com>
2019-09-30 00:37:58 +0530

    component part 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from "./shared/dishes";
 
 /*function App() {
   return (
@@ -26,6 +27,12 @@ import './App.css';
 }*/
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div>
@@ -34,7 +41,7 @@ class App extends Component {
             <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
