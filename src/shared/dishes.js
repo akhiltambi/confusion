cfa03277a7e16665506a2efc48ea558571204387
Akhil Tambi <akhiltambi@gmail.com>
2019-09-30 22:02:18 +0530

    Implemented and completed week one assignment

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -12,7 +12,7 @@ export const DISHES =
             {
             id: 0,
             rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
+            comment: "Imagine all the eatables, living in conFusion! Uthappizza",
             author: "John Lemon",
             date: "2012-10-16T17:57:28.556094Z"
             },
@@ -57,7 +57,7 @@ export const DISHES =
             {
             id: 0,
             rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
+            comment: "Imagine all the eatables, living in conFusion! Zucchipakoda",
             author: "John Lemon",
             date: "2012-10-16T17:57:28.556094Z"
             },
@@ -103,7 +103,7 @@ export const DISHES =
             {
             id: 0,
             rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
+            comment: "Imagine all the eatables, living in conFusion! Vadonut",
             author: "John Lemon",
             date: "2012-10-16T17:57:28.556094Z"
             },
@@ -149,7 +149,7 @@ export const DISHES =
             {
             id: 0,
             rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
+            comment: "Imagine all the eatables, living in conFusion! Elaicheese",
             author: "John Lemon",
             date: "2012-10-16T17:57:28.556094Z"
             },
