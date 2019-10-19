613ce65f896142ae2d28633f11e784d5e77931f2
Akhil Tambi <akhiltambi@gmail.com>
2019-10-19 12:41:23 +0530

    Single page part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -37,12 +37,25 @@ class Main extends Component {
             );
         };
 
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail 
+                    dish={this.state.dishes.filter((dish)=> {
+                        return dish.id === parseInt(match.params.dishId, 10)
+                    })[0]}
+                    comments={this.state.comments.filter((comment)=>comment.dishId === parseInt(match.params.dishId, 10))}>
+                </DishDetail>
+            );
+
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
